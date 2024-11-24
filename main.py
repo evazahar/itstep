9ffed24c79b2d02e537a22d7b45086924c21279c
evazahar <evazahar37@gmail.com>
2024-11-24 20:14:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("ви всі молодці")
+class ProgrammingLanguage:
+    def  init (self, name):
+self.name=name
+
+    def display greeting(self):
+         print (f"Hello! I am {self.name), one of the best programming languages!")
+
+class Python(Programming Language):
+    def   init   (self):
+        super().init ("Python")
+
+class Java(ProgrammingLanguage):
+    def    init   (self) :
+        super().init ("Java")
+class CPlusPlus(Programming Language):
+    def    init   (self):
+        super(). init ("C++")
+ifname== " main "=
+   python =Python()
+   java =Java()
+   cpp=CPlusPlus()
+
+python.display_greeting()
+java.display greeting()
+cpp.display_greeting()
