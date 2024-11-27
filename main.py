68c75f54e4595e50417e82f66ea3a57b89206efe
evazahar <evazahar37@gmail.com>
2024-11-27 17:01:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,70 +1,43 @@
-import random
-class Human:
-    def __init_(self, name "None", job-None, home-Hone, car-None):
-        self.name = name
-        self.money = 100
-        self.gladness = 50
-        self.satiety = 50
-        self.job = job
-        self.home = home
-        self.car = car
-    def get job(self):
-        if self.car.drive():
-            pass
-        else:
-            self.to_repair()
-            return
-        self.job = Job(job_list)
-    def get home(self):
-        self.home = House()
-    def get_car(self):
-        self.car = Auto(brands_of_car)
-    def pet(self, pet):
-        print(f"(self.name) strokes (pet.name). (pet.name) look happy!")
+#1
+age Int(input("Enter your age: "))
 
-    def talk to(self, pet):
-        print(f"(self.name) talk with (pet.name): 'How do you do, (pet.name}?*")
-        pet.interact(self)
-class House:
-    def  init (self):
-       self.mess = 0
-       self.food = 0
+if age 18:
 
- class Auto:
-     def   init(self, brand_list):
-           self.brand = random.choice(list(brand_list))
-           self.brand = random.choice(list(brand_list))
+print("You can enter the site!")
+if age <18:
 
+print("You cannot enter the site!")
 
+#2
+import randon
 
+def guess_the_number()1
 
+number_to_guess rondom.rendint(1, 10)
+attempts=0
+print("Bit! I guess a number from 1 to 10. Try to guess it.")
 
-       return False
-class Job:
-     def init (self, job list):
-        self.job = random.choice(list(job_list))
-        self.salary = job_list[self.job]["salary"]
-        self.gladness = job_list[self.job][ "gladness"]
- class Pet:
-     def  init (self, name, species):
-         self.name = name
-         self.species = species
+while True:
+user_input Input("Enter your number:
 
-     def interact(self, human):
-         print(f"(self.name) (the (self.species)) worries, when (human.name) move!")
+If not user Input.isdigit(): 
+print("Please enter an integer.")
+continue
 
+user_guess int(user_input)
+attempts+= 1
 
-brands_of_car = ("BMW":"fuel:100, "strength":100, "consumption": 6},
-"Lada":{"fuel":58, "strength":48, "consumption: 10},
-"Volve": "fuel": 70, "strength":150, "consumption": 8),
-"Ferrari": ("fuel":88, "strength":120, "consumption": 14},}
+elif if user_guess<number_to_guess:
 
-job list = ("Java developer":{"salary" :50, "gladness less": 10 },
-"Python developer": ["salary":40, gladness lesk: 31,
-"C++ developer":{"salary":45, "gladness_less":1,
-"Rust developer": ["salary": 70, "gladness_less": 1),)
+print("Requested number is greater.")
 
-dog = Pet ("Rex", "Dog")
-person = Human("None", "None")
-person.pet(dog)
-person.talk_to(dog)
+print("Requested number is less.")
+
+else:
+
+print(f"Congratulations! You guessed the number (number_to_guess) 3 (attempts) cпpo
+break
+
+if__name__=="__main__":
+
+guess the_number()
