781fdf16f3efc5251323d82dcef7f1dfe8ab0d65
evazahar <evazahar37@gmail.com>
2024-11-27 16:46:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,70 @@
-num1= float(input("Enter the first number: "))
-num2= float(input("Enter the second number: "))
-num3= float(input("Enter the third number: "))
+import random
+class Human:
+    def __init_(self, name "None", job-None, home-Hone, car-None):
+        self.name = name
+        self.money = 100
+        self.gladness = 50
+        self.satiety = 50
+        self.job = job
+        self.home = home
+        self.car = car
+    def get job(self):
+        if self.car.drive():
+            pass
+        else:
+            self.to_repair()
+            return
+        self.job = Job(job_list)
+    def get home(self):
+        self.home = House()
+    def get_car(self):
+        self.car = Auto(brands_of_car)
+    def pet(self, pet):
+        print(f"(self.name) strokes (pet.name). (pet.name) look happy!")
 
-result = num1*num2*num3
+    def talk to(self, pet):
+        print(f"(self.name) talk with (pet.name): 'How do you do, (pet.name}?*")
+        pet.interact(self)
+class House:
+    def  init (self):
+       self.mess = 0
+       self.food = 0
 
-print("Multiplication result:", result)
+ class Auto:
+     def   init(self, brand_list):
+           self.brand = random.choice(list(brand_list))
+           self.brand = random.choice(list(brand_list))
+
+
+
+
+
+       return False
+class Job:
+     def init (self, job list):
+        self.job = random.choice(list(job_list))
+        self.salary = job_list[self.job]["salary"]
+        self.gladness = job_list[self.job][ "gladness"]
+ class Pet:
+     def  init (self, name, species):
+         self.name = name
+         self.species = species
+
+     def interact(self, human):
+         print(f"(self.name) (the (self.species)) worries, when (human.name) move!")
+
+
+brands_of_car = ("BMW":"fuel:100, "strength":100, "consumption": 6},
+"Lada":{"fuel":58, "strength":48, "consumption: 10},
+"Volve": "fuel": 70, "strength":150, "consumption": 8),
+"Ferrari": ("fuel":88, "strength":120, "consumption": 14},}
+
+job list = ("Java developer":{"salary" :50, "gladness less": 10 },
+"Python developer": ["salary":40, gladness lesk: 31,
+"C++ developer":{"salary":45, "gladness_less":1,
+"Rust developer": ["salary": 70, "gladness_less": 1),)
+
+dog = Pet ("Rex", "Dog")
+person = Human("None", "None")
+person.pet(dog)
+person.talk_to(dog)
